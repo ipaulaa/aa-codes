94bca305a7366a452caccf579c226d76d4e7a5bb
Paula Rodrigues <ipaulaa.dev@gmail.com>
2024-11-14 13:21:22 -0300

    :octocat: data types

---
+++ 01_web-development-fundamentals/01_js-fundamentals/01_data-types/02_numbers/exercise.js
@@ -0,0 +1,9 @@
+// Example:
+console.log(4 + 2.5); // 6.5
+
+// Write 5 more expressions below:
+console.log(3 + 7); // 10
+console.log(3 - 7); // -4
+console.log(3 * 7); // 21
+console.log(7 / 3); // 2.333...
+console.log(7 % 3); // 1
---
+++ 01_web-development-fundamentals/01_js-fundamentals/01_data-types/04_variables/practice.js
@@ -0,0 +1,23 @@
+// 1. Declare a variable called `firstName` and assign it your first name as a string:
+let firstName = "Paula";
+
+// 2. Declare a variable called `lastName` and assign it your last name as a string:
+let lastName = "Rodrigues";
+
+// 3. Declare a variable called `age`:
+let age;
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+console.log(firstName); // 'Paula'
+console.log(lastName); // 'Rodrigues'
+console.log(age); // undefined
+
+// 5. Assign the `age` variable to a number:
+age = 28;
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+console.log(firstName); // 'Paula'
+console.log(lastName); // 'Rodrigues'
+console.log(age); // 28
